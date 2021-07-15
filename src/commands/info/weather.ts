fe2547b6b2fb489332f56ede00710e98e2f0d1a0
Aaron Li-Mai <aaronmai@hotmail.com>
2021-07-15 08:58:32 -0600

    added weather command

---
+++ src/commands/info/weather.ts
@@ -0,0 +1,47 @@
+import { Command } from "../../interfaces/Command";
+import { log, setCooldown } from "../../utils/utils";
+import weather from "weather-js";
+import convert from "convert";
+
+export default {
+  name: "weather",
+  category: "Info",
+  cooldown: 15,
+  globalCooldown: true,
+  execute({ client, channel, userstate }) {
+    setCooldown(client, this, channel, userstate);
+    weather.find(
+      { search: "New York City, NY", degreeType: "C" },
+      (err: string, result: any) => {
+        if (err) {
+          log("ERROR", "./src/commands/info/weather.ts", err);
+          return client.say(
+            channel,
+            "/me An error has occureed. Please try again."
+          );
+        }
+
+        if (!result || !result[0]) {
+          return client.say(channel, "/me No results found.");
+        }
+
+        const { location, current } = result[0];
+        const { winddisplay, temperature, windspeed, humidity } = current;
+        const fahrenheit = convertToFahrenheit(temperature);
+        const miles = convertToMiles(windspeed.split(" ")[0]);
+        const direction = winddisplay.split(" ").splice(-1)[0];
+        const text = `Weather for ${location.name}: ${current.skytext} with a temperature of ${current.temperature}°C (${fahrenheit}°F). Wind is blowing from the ${direction} at ${windspeed} (${miles} m/h) and the humidity is ${humidity}%.`;
+
+        return client.say(channel, `/me ${text}`);
+      }
+    );
+  }
+} as Command;
+
+const convertToFahrenheit = (temp: number) => {
+  return convert(temp, "C").to("F").toFixed(1);
+};
+
+const convertToMiles = (windspeed: number) => {
+  return convert(windspeed, "kilometers").to("miles").toFixed(1);
+};
