fa127f3312580a97f2a55e676fedd8ae6502f619
Aaron Li-Mai <aaronmai@hotmail.com>
2021-07-15 08:21:04 -0600

    added dadjoke command

---
+++ src/commands/misc/dadjoke.ts
@@ -0,0 +1,25 @@
+import { Command } from "../../interfaces/Command";
+import fetch from "node-fetch";
+import { log, setCooldown } from "../../utils/utils";
+
+export default {
+  name: "dadjoke",
+  category: "Misc",
+  cooldown: 15,
+  async execute({ client, channel, userstate }) {
+    try {
+      setCooldown(client, this, channel, userstate);
+      const resp = await fetch("https://icanhazdadjoke.com/", {
+        method: "GET",
+        headers: {
+          Accept: "application/json"
+        }
+      });
+      const data = await resp.json();
+      return client.say(channel, `/me ${data.joke}`);
+    } catch (e) {
+      log("ERROR", "./src/commands/misc/dadjoke.ts", e.message);
+      return client.say(channel, "An error has occurred. Please try again.");
+    }
+  }
+} as Command;
