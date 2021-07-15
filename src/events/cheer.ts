3f7cdade4fdcba0dc4ee3bda949e719c8a0374f6
Aaron Li-Mai <aaronmai@hotmail.com>
2021-07-15 11:43:33 -0600

    added cheer event

---
+++ src/events/cheer.ts
@@ -0,0 +1,18 @@
+import { Client } from "../Client";
+import { Userstate } from "tmi.js";
+import { formatNumber } from "../utils/utils";
+
+export default async (
+  client: Client,
+  channel: string,
+  userstate: Userstate,
+  _message: string
+) => {
+  const numBits = formatNumber(userstate.bits);
+  return client.say(
+    channel,
+    `/me Thank you @${userstate["display-name"]} for the ${numBits} bit${
+      userstate.bits != 1 ? "s" : ""
+    }!`
+  );
+};
