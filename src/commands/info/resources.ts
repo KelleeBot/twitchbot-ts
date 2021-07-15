61fcef97c8346c07a2e2c21194b7c233edcc039a
Aaron Li-Mai <aaronmai@hotmail.com>
2021-07-15 09:30:58 -0600

    added resources command

---
+++ src/commands/info/resources.ts
@@ -0,0 +1,16 @@
+import { Command } from "../../interfaces/Command";
+import { setCooldown } from "../../utils/utils";
+
+export default {
+  name: "resources",
+  category: "Info",
+  cooldown: 15,
+  globalCooldown: true,
+  execute({ client, channel, userstate }) {
+    setCooldown(client, this, channel, userstate);
+    return client.say(
+      channel,
+      `/me Here is a list of resources to help BIPOC and LGBTQ+ communities. If you have more resources, please DM Kéllee. https://kellee.carrd.co/`
+    );
+  }
+} as Command;
