44225807a2858b000c07411566ae0b6ba25e6f45
Aaron Li-Mai <aaronmai@hotmail.com>
2021-07-15 09:10:31 -0600

    added followage command

---
+++ src/commands/info/followage.ts
@@ -0,0 +1,35 @@
+import { Command } from "../../interfaces/Command";
+import fetch from "node-fetch";
+import {
+  errorMessage,
+  log,
+  replaceChars,
+  setCooldown
+} from "../../utils/utils";
+
+export default {
+  name: "followage",
+  aliases: ["fa"],
+  category: "Info",
+  cooldown: 15,
+  async execute({ client, channel, userstate }) {
+    try {
+      setCooldown(client, this, channel, userstate);
+      const resp = await fetch(
+        `https://beta.decapi.me/twitch/followage/${process.env.CHANNEL_NAME}/${userstate.username}?precision=7`
+      );
+      const data = await resp.text();
+      if (replaceChars(data) == "a user cannot follow themself") {
+        return client.say(channel, `/me ${data}`);
+      }
+
+      return client.say(
+        channel,
+        `/me ${userstate["display-name"]} has been following ${process.env.CHANNEL_NAME} for ${data}.`
+      );
+    } catch (e) {
+      log("ERROR", ".src/commands/info/followage.ts", e.message);
+      return errorMessage(client, channel);
+    }
+  }
+} as Command;
