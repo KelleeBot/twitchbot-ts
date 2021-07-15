8954769a1c805f96c7b2f45495ac012a5ea76e79
Aaron Li-Mai <aaronmai@hotmail.com>
2021-07-15 12:17:22 -0600

    added subgift event

---
+++ src/events/subgift.ts
@@ -0,0 +1,27 @@
+import { Userstate } from "tmi.js";
+import { Client } from "../Client";
+
+export default (
+  client: Client,
+  channel: string,
+  username: string,
+  streakMonths: number,
+  recipient: string,
+  methods: any,
+  userstate: Userstate
+) => {
+  if (
+    userstate["msg-param-recipient-display-name"].toLowerCase() ===
+    `${process.env.BOT_USERNAME}`.toLowerCase()
+  ) {
+    return client.say(
+      channel,
+      `/me Thank you @${username} for gifting a sub to me. I really appreciate it. kellee1Love`
+    );
+  }
+
+  return client.say(
+    channel,
+    `/me Thank you @${username} for gifting a sub to ${userstate["msg-param-recipient-display-name"]}! PrideRise`
+  );
+};
