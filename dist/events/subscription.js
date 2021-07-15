58936dbb81194b1e7470c3f78146a90c9d372d59
Aaron Li-Mai <aaronmai@hotmail.com>
2021-07-15 12:18:34 -0600

    added subscription event

---
+++ dist/events/subscription.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = (client, channel, username, methods, userstate) => {
+    return client.say(channel, `/me Thank you @${username} for subscribing! kellee1Love`);
+};
---
+++ src/events/subscription.ts
@@ -0,0 +1,15 @@
+import { Userstate } from "tmi.js";
+import { Client } from "../Client";
+
+export default (
+  client: Client,
+  channel: string,
+  username: string,
+  methods: any,
+  userstate: Userstate
+) => {
+  return client.say(
+    channel,
+    `/me Thank you @${username} for subscribing! kellee1Love`
+  );
+};
