4303641ffd4f1c801dd7335516b819b3b165f535
Aaron Li-Mai <aaronmai@hotmail.com>
2021-07-15 09:50:17 -0600

    added time command

---
+++ dist/commands/info/time.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("../../utils/utils");
+const dayjs_1 = __importDefault(require("dayjs"));
+const utc_1 = __importDefault(require("dayjs/plugin/utc"));
+const timezone_1 = __importDefault(require("dayjs/plugin/timezone"));
+const advancedFormat_1 = __importDefault(require("dayjs/plugin/advancedFormat"));
+dayjs_1.default.extend(utc_1.default);
+dayjs_1.default.extend(timezone_1.default);
+dayjs_1.default.extend(advancedFormat_1.default);
+exports.default = {
+    name: "time",
+    category: "Info",
+    cooldown: 15,
+    globalCooldown: true,
+    execute({ client, channel, userstate }) {
+        utils_1.setCooldown(client, this, channel, userstate);
+        const time = dayjs_1.default()
+            .tz("America/New_York")
+            .format("DD/MM/YYYY h:mm:ss A z");
+        return client.say(channel, `/me Kéllee's current time is ${time}.`);
+    }
+};
---
+++ src/commands/info/time.ts
@@ -0,0 +1,23 @@
+import { Command } from "../../interfaces/Command";
+import { setCooldown } from "../../utils/utils";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import advanced from "dayjs/plugin/advancedFormat";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(advanced);
+
+export default {
+  name: "time",
+  category: "Info",
+  cooldown: 15,
+  globalCooldown: true,
+  execute({ client, channel, userstate }) {
+    setCooldown(client, this, channel, userstate);
+    const time = dayjs()
+      .tz("America/New_York")
+      .format("DD/MM/YYYY h:mm:ss A z");
+    return client.say(channel, `/me Kéllee's current time is ${time}.`);
+  }
+} as Command;
