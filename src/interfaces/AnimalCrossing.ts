14f80656a6ee34fcb96c4b250de87c8733686610
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-04 19:24:43 -0700

    added Villagera interface

---
+++ src/interfaces/AnimalCrossing.ts
@@ -0,0 +1,22 @@
+export interface Villagers {
+    name: string;
+    url: string;
+    alt_name: string;
+    title_color: string;
+    text_color: string;
+    id: string;
+    image_url: string;
+    species: string;
+    personality: string;
+    gender: string;
+    birthday_month: string;
+    birthday_day: string;
+    sign: string;
+    quote: string;
+    phrase: string;
+    clothing: string;
+    islander: boolean;
+    debut: string;
+    prev_phrases: string[];
+    appearances: string[];
+}
