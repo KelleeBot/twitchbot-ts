c5108b15df89d66bd6ead3c783915b42cee8f3a1
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-04 19:25:15 -0700

    use string-similarity package to determine closest villager name if typo

--- src/commands/AC/villager.ts
+++ src/commands/AC/villager.ts
@@ -1,6 +1,10 @@
 import fetch from "node-fetch";
+import stringSimilarity from "string-similarity";
+import { Villagers } from "../../interfaces";
 import { Command } from "../../interfaces/Command";
 import { log, setCooldown } from "../../utils/utils";
+
+let villagers: string[] = [];
 
 export default {
     name: "villager",
@@ -14,42 +18,69 @@ export default {
         }
     ],
     async execute({ client, userstate, channel, args }) {
-        setCooldown(client, this, channel, userstate);
-        let query = args.join(" ").trim();
+        let query = args.join(" ").toLowerCase().trim();
         if (query.includes(" ")) {
             query = query.replace(/ +/g, "_");
         }
 
-        if (query.toLowerCase() === "etoile") {
-            query = "Étoile";
-        }
+        const data = await fetchVillagerName(query);
+        if (!data.length) {
+            const matches = stringSimilarity.findBestMatch(query, await fetchAllVillagerNames());
+            const options = matches.ratings
+                .filter((v) => v.rating >= 0.3)
+                .sort((a, b) => b.rating - a.rating)
+                .slice(0, Math.min(5, matches.ratings.length));
 
-        try {
-            const resp = await fetch(
-                `https://api.nookipedia.com/villagers?name=${encodeURIComponent(
-                    query.toLowerCase()
-                )}&nhdetails=true`,
-                {
-                    method: "GET",
-                    headers: {
-                        "X-API-KEY": `${process.env.NOOK_API_KEY}`,
-                        "Accept-Version": "2.0.0"
-                    }
-                }
-            );
+            if (!options.length)
+                return client.say(channel, `/me I couldn't find that villager kellee1Cry`);
 
-            const data = await resp.json();
-            if (!data)
-                return client.say(channel, "/me I could not find a villager with that name.");
-
-            const { name, personality, species, phrase, url } = data[0];
+            const { bestMatch } = matches;
             return client.say(
                 channel,
-                `/me ${name} is a ${personality.toLowerCase()} ${species.toLowerCase()}, ${phrase}! More info: ${url}`
+                `/me I could not find a villager with that name. Maybe you meant ${bestMatch.target.replace(
+                    /\b(\w)/g,
+                    (char) => char.toUpperCase()
+                )}?`
             );
-        } catch (e) {
-            log("ERROR", `${__filename}`, `An error has occurred: ${e}`);
-            return client.say(channel, "/me I could not find a villager with that name.");
         }
+
+        setCooldown(client, this, channel, userstate);
+        const { name, personality, species, phrase, url } = data[0];
+        return client.say(
+            channel,
+            `/me ${name} is a ${personality.toLowerCase()} ${species.toLowerCase()}, ${phrase}! More info: ${url}`
+        );
     }
 } as Command;
+
+const fetchAllVillagerNames = async () => {
+    if (villagers.length) return villagers;
+
+    const resp = await fetch("https://api.nookipedia.com/villagers", {
+        method: "GET",
+        headers: {
+            "X-API-KEY": `${process.env.NOOK_API_KEY}`,
+            "Accept-Version": "2.0.0"
+        }
+    });
+    const data = await resp.json();
+    villagers = data.map((villager: Villagers) => villager.name.toLowerCase());
+
+    return villagers;
+};
+
+const fetchVillagerName = async (name: string) => {
+    const resp = await fetch(
+        `https://api.nookipedia.com/villagers?name=${encodeURIComponent(
+            name.toLowerCase()
+        )}&nhdetails=true`,
+        {
+            method: "GET",
+            headers: {
+                "X-API-KEY": `${process.env.NOOK_API_KEY}`,
+                "Accept-Version": "2.0.0"
+            }
+        }
+    );
+    return await resp.json();
+};
