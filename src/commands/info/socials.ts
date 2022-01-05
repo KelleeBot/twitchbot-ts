6e16fe7af8b4b8ca1c71d2d1cd700e3139b0762f
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-04 18:24:18 -0700

    updated socials command

--- src/commands/info/socials.ts
+++ src/commands/info/socials.ts
@@ -3,19 +3,15 @@ import { setCooldown } from "../../utils/utils";
 
 export default {
     name: "socials",
+    aliases: ["social"],
     category: "Info",
     cooldown: 15,
     globalCooldown: true,
     execute({ client, channel, userstate }) {
         setCooldown(client, this, channel, userstate);
-        client.say(channel, "/me Follow me on Twitter! https://twitter.com/kelleelune");
-        // client.say(
-        //   channel,
-        //   `/me Don't forget to subscribe to me on YouTube as well! https://www.youtube.com/channel/UCQymVHcUQZ3fzHMZMZ-v0Vw`
-        // );
-        client.say(
+        return client.say(
             channel,
-            "/me I also have an Instagram account where I post about Animal Crossing a lot! https://www.instagram.com/kelleelune/"
+            "/me You can find me on social media by clicking on this Linktree link! https://www.linktr.ee/kellee_vt"
         );
     }
 } as Command;
