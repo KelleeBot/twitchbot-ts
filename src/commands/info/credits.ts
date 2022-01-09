518ed689f45aacafe5ffc70363f172c465a22db1
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-09 12:47:00 -0700

    renamed resources to credits

--- src/commands/info/credits.ts
+++ src/commands/info/credits.ts
@@ -2,7 +2,7 @@ import { Command } from "../../interfaces/Command";
 import { setCooldown } from "../../utils/utils";
 
 export default {
-    name: "resources",
+    name: "credits",
     category: "Info",
     cooldown: 15,
     globalCooldown: true,
@@ -10,7 +10,7 @@ export default {
         setCooldown(client, this, channel, userstate);
         return client.say(
             channel,
-            `/me Here is a list of resources to help BIPOC and LGBTQ+ communities. If you have more resources, please DM Kéllee. https://kellee.carrd.co/`
+            `/me https://kellee.carrd.co/`
         );
     }
 } as Command;
