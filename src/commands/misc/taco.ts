12a4740302b3b6b17accd3061cb8ce95d4b6c1d5
Aaron Li-Mai <aaronmai@hotmail.com>
2022-01-19 18:09:19 -0700

    added taco command

---
+++ src/commands/misc/taco.ts
@@ -0,0 +1,13 @@
+import { Command } from "../../interfaces/Command";
+import { setCooldown } from "../../utils/utils";
+
+export default {
+    name: "taco",
+    aliases: ["🌮"],
+    category: "Misc",
+    cooldown: 15,
+    execute({ client, channel, userstate }) {
+        setCooldown(client, this, channel, userstate);
+        return client.say(channel, "/me Today is Taco Tuesday!!! With guest @7Squish! Please go give them a follow over at https://www.twitch.tv/7squish!");
+    }
+} as Command;
