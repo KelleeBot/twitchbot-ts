fab05677a93a63492c2a190be75552b349c83d44
aaron <aaronmai@hotmail.com>
2022-06-14 17:07:43 -0600

    added setTimeout when shouting out

--- src/events/raided.ts
+++ src/events/raided.ts
@@ -5,15 +5,17 @@ import { log } from "../utils/utils";
 export default async (client: Client, channel: string, username: string, viewers: number) => {
     client.say(
         channel,
-        `/me Incoming raid! PrideRise Thank you @${username} for raiding the channel with ${viewers} viewer${
-            viewers !== 1 ? "s" : ""
+        `/me Incoming raid! PrideRise Thank you @${username} for raiding the channel with ${viewers} viewer${viewers !== 1 ? "s" : ""
         }! Welcome raiders!`
     );
     const game = await gamePlayed(username);
-    return client.say(
-        channel,
-        `/me kellee1Love Be sure to show ${username} some love and follow them at https://www.twitch.tv/${username.toLowerCase()} They were last playing ${game} kellee1Love`
-    );
+
+    setTimeout(() => {
+        return client.say(
+            channel,
+            `/me kellee1Love Be sure to show ${username} some love and follow them at https://www.twitch.tv/${username.toLowerCase()} They were last playing ${game} kellee1Love`
+        )
+    }, 5_000);
 };
 
 const gamePlayed = async (username: string) => {
